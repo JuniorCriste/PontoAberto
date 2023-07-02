24759fe34db389c06b40b64071781cfdeb3b7572
JuniorCriste <informaticode@gmail.com>
2023-07-01 22:05:44 -0300

    0.01

---
+++ pascal/unit1.pas
@@ -0,0 +1,26 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TFistScreen = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  FistScreen: TFistScreen;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
